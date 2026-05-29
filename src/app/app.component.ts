73438d5be73ff5894e319f0eb5ad9463c788769f
Metalquill <65878989+Metalquill@users.noreply.github.com>
2026-05-29 13:49:41 +1200

    Updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { TestarooneyComponent } from './component/testarooney/testarooney.component';
 import { UserComponent } from './component/user/user.component'
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { WeatherComponent } from './component/weather/weather.component';
+
+
+ interface empInterface {
+    id: number;
+    name: string;
+    position: string;
+}
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, TestarooneyComponent, UserComponent],
+    imports: [RouterOutlet, RouterLink, TestarooneyComponent, UserComponent, FormsModule, CommonModule, WeatherComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
+    styleUrls: ['./app.component.css'],
     standalone: true,
 })
 export class AppComponent {
   title = 'testangularapp';
+  wordOutput = "";
 
   theDict = { "title": "cat"}
   bruh = this.theDict.title
@@ -22,4 +33,100 @@ export class AppComponent {
   inputValue: string = "Bonjour";
   otherInputValue: string = "Hola";
   isDisabled: boolean = false;
+  displayExample: boolean = true;
+  nums: number = 20;
+  displayElement: boolean = false;
+  showEmployees: boolean = true;
+  age: number = 18;
+
+  items: string[] = ['Apple', 'Banana', 'Orange', 'Mango'];
+  fullStackDev = [
+    { id: 1,
+      name: 'Angular',
+    },
+    {
+      id: 2,
+      name: 'React',
+    },
+    {      
+      id: 3,
+      name: 'Vue',
+    },
+    {
+      id: 4,
+      name: 'Node.js',
+    }
+  ]
+
+ 
+
+  employees = signal<empInterface[]>([
+    {id: 1, name: 'John Doe', position: 'Software Engineer' },
+    {id: 2, name: 'Jane Smith', position: 'Product Manager' },
+    {id: 3, name: 'Alice Johnson', position: 'UX Designer' }
+  ]);
+
+  toggleEmployees() {
+    this.showEmployees = !this.showEmployees;
+  }
+
+
+  display(msg: string) {
+    alert(msg);
+  }
+
+  count = 0;
+  counter (type: string) {
+    type === 'increment' ? this.count++ : this.count--;
+  }
+
+  newCount = 0;
+  incr() {
+    this.newCount++;
+  }
+
+  dragCount = 0;
+  onDrag() {
+    this.dragCount++;
+  }
+
+  onKeyPress(e: any) {
+    // this.wordOutput += e.target.value;
+    this.wordOutput += e.key;
+  }
+
+  isShift(event: any) {
+    if (event.shiftKey && event.key === 'Y') {
+      console.log('Shift + Y is pressed', event)
+    }
+
+  }
+
+  nextCount = 0;
+  // Handle keyboard events for arrow keys
+  onKeyDown(event: KeyboardEvent) { 
+    if(event.key === 'ArrowUp') {
+      this.nextCount++;
+    } else if (event.key === 'ArrowDown') {
+      this.nextCount--;
+  };
 }
+
+staticInput: string = "Static Input";
+dynamicInput: string = '';
+quantity: number = 1;
+pricePerItem: number = 16.99;
+
+get totalPrice(): number {
+  const myTotal = this.quantity * this.pricePerItem;
+  if (myTotal > 1000 && myTotal < 10000) {
+    return parseFloat(myTotal.toPrecision(6));
+  }
+  return parseFloat(myTotal.toFixed(4));
+
+}
+}
+
+
+
+// ng serve --port 4401 
