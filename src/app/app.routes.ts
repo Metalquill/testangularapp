55a5f813b2cdd92c9c816f6879e3a23161b03a36
Metalquill <65878989+Metalquill@users.noreply.github.com>
2026-05-29 16:17:50 +1200

    Updated app for the second time today - 29-05-2026

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { WeatherComponent } from './component/weather/weather.component';
+import { TestarooneyComponent } from './component/testarooney/testarooney.component';
+import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
   { path: 'weather', component: WeatherComponent },
-  { path: '', redirectTo: '/weather', pathMatch: 'full' }
+  { path: 'testarooney', component: TestarooneyComponent },
+  { path: '', redirectTo: '/testarooney', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
 ];
